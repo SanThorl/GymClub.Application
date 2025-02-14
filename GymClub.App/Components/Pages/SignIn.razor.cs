bd5684f089713cb954808b5176356fe725ae56b8
ahthawm <santawmmaran@acedatasystems.com>
2025-02-15 02:04:09 +0630

    feat : add devcode for json serialization

--- GymClub.App/Components/Pages/SignIn.razor.cs
+++ GymClub.App/Components/Pages/SignIn.razor.cs
@@ -1,18 +1,25 @@
 ﻿using GymClub.Domain.Features.User.Login;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components;
 
 namespace GymClub.App.Components.Pages
 {
     public partial class SignIn
     {
+        private string _cookieDataStr { get; set; } = "";
+        [Inject] private AuthenticationStateProvider authStateProvider { get; set; }
+
         private LoginModel _reqModel = new LoginModel();
 
-        //protected override Task OnAfterRenderAsync(bool firstRender)
-        //{
-        //    if (firstRender)
-        //    {
-
-        //    }
-        //}
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await _injectService.EnableLoading();
+                //var customAuthStateProvider = (CustomAuthenticationStateProvider)_authStateProvider;
+                await _injectService.DisableLoading();
+            }
+        }
 
         private void LogIn()
         {
---
+++ GymClub.Shared/DevCode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GymClub.Shared
+{
+    public static partial class DevCode
+    {
+        public static string? ToJson<T>(this T? obj, bool format = false)
+        {
+            if (obj == null) return string.Empty;
+            string? result;
+            if (obj is string)
+            {
+                result = obj.ToString();
+                goto Result;
+            }
+
+            var settings = new JsonSerializerSettings { DateFormatString = "yyyy-MM-ddTHH:mm:ss.sssZ" };
+            result = format
+                ? JsonConvert.SerializeObject(obj, Newtonsoft.Json.Formatting.Indented, settings)
+                : JsonConvert.SerializeObject(obj, settings);
+        Result:
+            return result;
+        }
+
+        public static T? ToObject<T>(this string? jsonStr)
+        {
+            try
+            {
+                if (jsonStr != null)
+                {
+                    var test = JsonConvert.DeserializeObject<T>(jsonStr,
+                        new JsonSerializerSettings { DateParseHandling = DateParseHandling.DateTimeOffset });
+                    return test;
+                }
+            }
+            catch
+            {
+                return (T)Convert.ChangeType(jsonStr, typeof(T))!;
+            }
+
+            return default;
+        }
+    }
+}
