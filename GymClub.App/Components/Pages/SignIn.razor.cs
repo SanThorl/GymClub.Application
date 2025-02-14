c233a95d4f3eb982a4006faf9a607a1a4c90944f
ahthawm <santawmmaran@acedatasystems.com>
2025-02-14 14:55:42 +0630

    feat : 1st page

---
+++ GymClub.App/Components/Pages/SignIn.razor.cs
@@ -0,0 +1,18 @@
+﻿using GymClub.Domain.Features.User.Login;
+
+namespace GymClub.App.Components.Pages
+{
+    public partial class SignIn
+    {
+        private LoginModel _reqModel = new LoginModel();
+        void SignUp()
+        {
+            _nav.NavigateTo("/register");
+        }
+
+        void Login()
+        {
+            _nav.NavigateTo("/signIn");
+        }
+    }
+}
--- GymClub.Domain/Features/User/Login/LoginService.cs
+++ GymClub.Domain/Features/User/Login/LoginService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GymClub.Database.DbModels;
 
 namespace GymClub.Domain.Features.User.Login
 {
-    internal class LoginService
+    public class LoginService
     {
+        private readonly AppDbContext _db;
+
+        public LoginService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        //public async Task<Login>
     }
 }
